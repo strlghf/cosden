6520fd7dcdda58a5df7a44cd398cefba8277718c
strlghf <strlghf@gmail.com>
2026-03-21 08:57:53 -0300

    updating abortController

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import { Demo } from './fetchingData'
 
 function App() {
   return (
     <>
+      <Demo />
     </>
   )
 }
--- src/fetchingData/index.tsx
+++ src/fetchingData/index.tsx
@@ -8,42 +8,58 @@ interface Post {
   title: string;
 }
 
-export default function Demo () {
+export function Demo () {
   const [posts, setPosts] = useState<Post[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState();
+  const [error, setError] = useState<Error | null>();
   const [page, setPage] = useState(0);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const fetchPosts = async() => {
       setIsLoading(true);
       try {
-        const response = await fetch(`${BASE_URL}/posts?page=${page}`);
+        const response = await fetch(`${BASE_URL}/posts?page=${page}`, controller);
+        
+        if (!response.ok) throw new Error("Error fetching data");
+
         const posts = (await response.json()) as Post[];
         setPosts(posts);
+        setError(null);
       } catch (e: any) {
+        if (e.name === "AbortError") {
+          console.log("Aborted"); 
+          return;
+        }
         setError(e)
       } finally {
         setIsLoading(false);
       }
     }
     fetchPosts();
-  }, [page]);
 
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
+    return () => controller.abort();
+  }, [page]);
 
   if (error) {
     return <div>Something went wrong! Please try again</div>
   }
 
+  function handleClick () {
+    setPage(p => p + 1);
+  }
+
   return (
     <div className="tutorial">
       <h1 className="mb-4 text-2xl">Data Fetching in React</h1>
-      <ul>
-        {posts.map(post => <li key={post.id}>{post.title}</li>)}
-      </ul>
+      <button onClick={handleClick}>Increase Page ({page})</button>
+        {isLoading && <div>Loading...</div> }
+        {!isLoading && 
+          (<ul>
+            {posts.map(post => <li key={post.id}>{post.title}</li>)}
+        </ul>)
+        }
     </div>
   )
 }
