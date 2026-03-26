89a90483794f195ac9b2d32c110e0d2a64734ed1
strlghf <strlghf@gmail.com>
2026-03-26 15:50:34 -0300

    redux tk

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import './App.css'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { Demo } from './keysLists'
-import { useState } from 'react';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    }
-  }
-});
 
 function App() {
-  const [showDemo, setShowDemo] = useState(true);
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <button onClick={() => setShowDemo(!showDemo)}>Toggle Demo</button>
-      {showDemo && <Demo />}
-    </QueryClientProvider>
+    <>
+    </>
   )
 }
 
